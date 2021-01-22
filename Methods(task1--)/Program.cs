2d38ff6becab48fd9a6ab0aec1aff7b46587524c
ilker Gürhan <ilkergs_@hotmail.com>
2021-01-22 16:35:31 +0300

    ödev1güncellemesi

---
+++ Methods(task1--)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods_task1___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add();
+            Add();
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!!!");
+        }
+    }
+}
