841481fedc9b8d13d762b1cb2bea3f18db7de34a
ilker Gürhan <ilkergs_@hotmail.com>
2021-01-22 16:31:47 +0300

    Ödev1-Eklendi

---
+++ Methods(task1)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Methods_task1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+    }
+}
