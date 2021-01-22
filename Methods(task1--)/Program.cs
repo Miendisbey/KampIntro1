28f38a8f399361258fbcd3ed84c136f77a755e97
ilker Gürhan <ilkergs_@hotmail.com>
2021-01-23 00:24:20 +0300

    00:24kapanış

--- Methods(task1--)/Program.cs
+++ Methods(task1--)/Program.cs
@@ -9,12 +9,21 @@ namespace Methods_task1___
             Add();
             Add();
             Add();
+            var result = Add2(20,30);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
 
         static void Add()
         {
             Console.WriteLine("Added!!!");
+        }
+
+        //void = git şunu yap demek.
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
         }
     }
 }
