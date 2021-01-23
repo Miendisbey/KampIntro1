e9903cc71f1f0324a04782190915095234d3ae26
ilker Gürhan <ilkergs_@hotmail.com>
2021-01-23 17:37:04 +0300

    17:36 23.01.2021 // Güncelleme

--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -6,9 +6,35 @@ namespace OOP1
 {
     class ProductManager
     {
-        public void Add(Product product)
+        public void Add(Product product) //Product türünde product.
         {
-
+            Console.WriteLine(product.ProductName + " eklendi.");
         }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+       
+        
+        
+        
+        
+        
+        //void --> Void metotları, emir kipinde git ekle, git güncelle.
+        //İşlem sonucunda ekstra bir bilgiye ihtiyacımız yoksa void tanımlıyoruz
+
+        //public int Topla(int sayi1, int sayi2)
+        //{
+        //    return sayi1 + sayi2;
+        //}
+
+        //public void Topla2(int sayi1, int sayi2)
+        //{
+        //    Console.WriteLine(sayi1 + sayi2);
+
+        //}
+        ////Topla2 işini yapıyor. sayi1 ile sayi2 yi topluyor ve onu ekrana yazdırıyor.
+        ////
     }
 }
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -16,9 +16,28 @@ namespace OOP1
             Product product2 = new Product {Id = 2, CategoryId = 5, 
                 UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35};
 
+            
+            //case sensitive
             //PascalCase   //camelCase
             ProductManager productManager = new ProductManager();
+            //"ProductManager" türündeki "productManager" 
+            //yeni bir referans numarası almış "ProductManager"dır demek.
             productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            
+            
+            
+            
+            
+            //productManager.Topla2(3, 6);
+
+            //int toplamaSonucu = productManager.Topla(3, 6);
+
+            //Console.WriteLine(toplamaSonucu * 2);
+            
+            ////Bu işlem sonucunu kullanmak istiyorum. Kullanamıyorum.(void(Topla2),int(Topla))
+            ////Toplam sonucunu kullanmak istiyorsan int kullanmalısın.
         }
     }
 }
