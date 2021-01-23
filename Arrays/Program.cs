901a43eaf4ba06b5ebaff7f99866326e6b0feb25
ilker Gürhan <ilkergs_@hotmail.com>
2021-01-23 13:26:32 +0300

    13:25 23.01.2021\\

---
+++ Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string student1 = "İlker";
+            string student2 = "Sena";
+            string student3 = "İlayda";
+            //ayrı ayrı değişkenler olduğu için yönetimi zor 
+            //"array" ile aynı tipteki değerleri değişkenleri tek bir noktada yönetip,
+            //tek bir değişkenle yönetme şeklinde ve ona hızlıca ulaşma, döngü ile dönme ihtiyacında
+            //array lerden yararlanırız
+
+            string[] students = new string[3];
+            students[0] = "İlker";
+            students[1] = "Sena";
+            students[2] = "İlayda";
+
+            string[] students2 = { "İlker", "Sena", "İlayda" };
+
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
